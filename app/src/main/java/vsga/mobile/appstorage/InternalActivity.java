93f388d623aca394e37fb7cfa1ed9485b4dc0003
setiawanalraz <alrazsetiawan79@gmail.com>
2021-08-12 15:01:57 +0700

    Menambahkan dan mengubah Class InternalActivity 1.2

--- app/src/main/java/vsga/mobile/appstorage/InternalActivity.java
+++ app/src/main/java/vsga/mobile/appstorage/InternalActivity.java
@@ -7,8 +7,11 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class InternalActivity extends AppCompatActivity implements View.OnClickListener{
     public static final String FILENAME = "namafile.txt";
@@ -49,6 +52,51 @@ public class InternalActivity extends AppCompatActivity implements View.OnClickL
         }
     }
 
+    void ubahFile() {
+        String ubah = "Update Isi Data File Text";
+        File file = new File(getFilesDir(), FILENAME);
+
+        FileOutputStream outputStream = null;
+        try {
+            file.createNewFile();
+            outputStream = new FileOutputStream(file, false);
+            outputStream.write(ubah.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void bacaFile() {
+        File sdcard = getFilesDir();
+        File file = new File(sdcard, FILENAME);
+
+        if (file.exists()) {
+            StringBuilder text = new StringBuilder();
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                while (line != null) {
+                    text.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+            } catch (IOException e) {
+                System.out.println("Error " + e.getMessage());
+            }
+            textBaca.setText(text.toString());
+        }
+    }
+
+    void hapusFile() {
+        File file = new File(getFilesDir(), FILENAME);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
     @Override
     public void onClick(View v) {
         jalankanPerintah(v.getId());
@@ -58,6 +106,15 @@ public class InternalActivity extends AppCompatActivity implements View.OnClickL
         switch (id) {
             case R.id.buttonBuatFile:
                 buatFile();
+                break;
+            case R.id.buttonBacaFile:
+                bacaFile();
+                break;
+            case R.id.buttonUbahFile:
+                ubahFile();
+                break;
+            case R.id.buttonHapusFile:
+                hapusFile();
                 break;
         }
     }
